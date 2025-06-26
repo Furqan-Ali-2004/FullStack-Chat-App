f0c5ec933e22d3a1b6fed972139ebe83e9902841
Furqan-Ali-2004 <furqangraphy2004@gmail.com>
2025-06-26 14:28:06 +0500

    testing

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [""],
+    origin: ["http://localhost:5173"],
   },
 });
 
@@ -15,25 +15,23 @@ export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// Used to store online users and their socket IDs
+// used to store online users
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+  console.log("A user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all connected clients
+  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+    console.log("A user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
-
-  // Handle other socket events here
 });
 
 export { io, app, server };
