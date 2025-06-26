76bd39f7d5accc5322de12ece6465745d9ed9428
Furqan-Ali-2004 <furqangraphy2004@gmail.com>
2025-06-26 13:25:12 +0500

    testing

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://fullstack-chat-app-1-9c4w.onrender.com"],
+    origin: ["http://localhost:5001"],
   },
 });
 
